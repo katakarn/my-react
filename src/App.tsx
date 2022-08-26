44db024bd764bbe848cb6022d475ebec127bd429
Katakarn Supserm <katakarn@hotmail.com>
2022-08-26 15:06:27 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { InputText } from "primereact/inputtext";
+
+export const Abc: React.FC = () => {
+  console.log('hi');
+  return <div>hi : Abc</div>;
+};
+
+export function Def() {
+  console.log('hi');
+  // return <div>hi : Def</div>;
+}
+
 
 function App() {
+  const [value1, setValue1] = React.useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.tsx</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <div>
+      <Abc/>
+      <InputText value={value1} onChange={(e) => setValue1(e.target.value)} />
     </div>
   );
 }
